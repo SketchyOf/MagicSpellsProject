f2996d4f8613319aabe2caefc1a67b1391f86baf
SketchyOf <sjachym01@gmail.com>
2025-06-27 13:14:47 +0200

    Update script.js

--- script.js
+++ script.js
@@ -14,4 +14,6 @@ const appearOnScroll = new IntersectionObserver((entries) => {
   });
 }, appearOptions);
 
-faders.forEach(fade => appearOnScroll.observe(fade));
+faders.forEach(fade => {
+  appearOnScroll.observe(fade);
+});
