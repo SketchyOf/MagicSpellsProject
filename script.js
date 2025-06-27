c3c51851f27b20e2e2020b2f803feff00dbbbfd3
SketchyOf <sjachym01@gmail.com>
2025-06-27 12:55:26 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
 const faders = document.querySelectorAll('.fade');
 
 const appearOptions = {
-  threshold: 0.2,
-  rootMargin: "0px 0px -50px 0px"
+  threshold: 0.2
 };
 
-const appearOnScroll = new IntersectionObserver(function(entries, appearOnScroll) {
+const appearOnScroll = new IntersectionObserver((entries) => {
   entries.forEach(entry => {
-    if (!entry.isIntersecting) return;
-    entry.target.classList.add('show');
-    appearOnScroll.unobserve(entry.target);
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    } else {
+      entry.target.classList.remove('show');
+    }
   });
 }, appearOptions);
 
-faders.forEach(fade => {
-  appearOnScroll.observe(fade);
-});
+faders.forEach(fade => appearOnScroll.observe(fade));
